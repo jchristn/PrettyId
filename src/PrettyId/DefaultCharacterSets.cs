e9fe226cdf0878ed7d434485107880866e95fe7b
Joel Christner <joel.christner@gmail.com>
2025-11-08 16:58:54 -0800

    feature/v2.0

--- src/PrettyId/DefaultCharacterSets.cs
+++ src/PrettyId/DefaultCharacterSets.cs
@@ -59,5 +59,17 @@ namespace PrettyId
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'a', 'b', 'c', 'd', 'e', 'f'
         };
+
+        /// <summary>
+        /// URL-safe characters (RFC 3986 unreserved characters).
+        /// Includes: a-z, A-Z, 0-9, hyphen, underscore, period, and tilde.
+        /// </summary>
+        public static char[] UrlSafe = new char[]
+        {
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '0','1','2','3','4','5','6','7','8','9',
+            '-','_','.','\u007e' // hyphen, underscore, period, tilde
+        };
     }
 }
